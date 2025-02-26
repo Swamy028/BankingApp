f7b37a37e7793436468078f7e5ccb5d76a8f6826
Swamy028 <165126905+Swamy028@users.noreply.github.com>
2025-02-26 11:55:36 +0530

    Delete backend directory

--- backend/routes/accountRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { createAccount, depositMoney, withdrawMoney, getBalance } = require('../controllers/accountController');
-const router = express.Router();
-router.post('/account', createAccount);
-router.post('/deposit/:id', depositMoney);
-router.post('/withdraw/:id', withdrawMoney);
-router.get('/balance/:id', getBalance);
-module.exports = router;
